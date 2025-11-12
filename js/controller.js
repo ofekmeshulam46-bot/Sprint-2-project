f1a9700a12e17da18044a97f667431faed06039f
Ofek Meshulam <ofekmeshulam46@gmail.com>
2025-11-12 12:48:55 +0200

    basic functions

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,25 @@
 'use strict'
+
+function renderMeme() {}
+
+function onInit() {
+  gElCanvas = document.querySelector('canvas')
+  gCtx = gElCanvas.getContext('2d')
+  resizeCanvas()
+}
+
+function onResize() {
+  resizeCanvas()
+  onClearCanvas()
+}
+
+function resizeCanvas() {
+  const elContainer = document.querySelector('.canvas-container')
+  gElCanvas.width = elContainer.offsetWidth
+  gElCanvas.height = elContainer.offsetHeight
+}
+
+function onClearCanvas() {
+  gCtx.fillStyle = '#ffffff'
+  gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height)
+}
